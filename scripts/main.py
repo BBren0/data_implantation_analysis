80517725de9dc4bb945c9e3b6f59cf4ddba21779
Bbren0 <breno430@outlook.com>
2024-02-20 17:00:08 -0300

    Commit 2

--- scripts/main.py
+++ scripts/main.py
@@ -8,12 +8,15 @@ def ler_arquvios_xls(pasta_dados):
             if arquivo.endswith('.xlsx'):
                 caminho_arquivo =  os.path.join(pasta_dados, arquivo)
                 print(f"Lendo arquivos: {caminho_arquivo}")
-                df = pd.read_excel(arquivo)
+                df = pd.read_excel(caminho_arquivo, sheet_name=None)
                 dados.append(df)
+            else:
+                print("Arquivo não encontrado:", caminho_arquivo)
+
         return dados
 
 def main():
-    pasta_dados = "C:\\Users\\Pointer 01\\Downloads\\data_implantation_analysis\\data"
+    pasta_dados = "data_implantation_analysis\\data"
     dados = ler_arquvios_xls(pasta_dados)
 
     if not pasta_dados:
@@ -24,9 +27,11 @@ def main():
         return
            
 
-    for df in dados:
-        print("DataFrame:")
-        print(df.info())
+    for dicionario in dados:
+        for nome_planilhas, df in dicionario.items():
+            print(f"\nDataFrame da planilha '{nome_planilhas}'")
+            print(df.head())
+            print("\n")
 
-    if __name__=="__main__":
-           main()
+if __name__=="__main__":
+    main()
